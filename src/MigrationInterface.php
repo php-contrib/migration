6df0aeb6298f18f14e69dc87b04c17c5c7e728b1
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-12-06 03:57:35 -0500

    docs: add comments

--- src/MigrationInterface.php
+++ src/MigrationInterface.php
@@ -6,9 +6,18 @@ namespace PhpContrib\Migration;
 
 use PDO;
 
+/**
+ * Enables automatic and on-demand management of data
+ */
 interface MigrationInterface
 {
+    /**
+     * Executes the migrations
+     */
     public function up(PDO $connection): MigrationRunStatus;
 
+    /**
+     * Rolls back an executed migration
+     */
     public function down(PDO $connection): MigrationRunStatus;
 }
